00a70618e292da3767044ded7787f52fde7d444b
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2020-09-02 22:56:04 +0200

    Code"cleanup" and comments added

--- backend/Harvesters/IcaHarvester.js
+++ backend/Harvesters/IcaHarvester.js
@@ -7,23 +7,27 @@ Store : name, websiteUrl, openingHours
 Address : country, region, city, zipCode, streetNumber, streetName
 */
 
-//https://handla.ica.se/api/content/v1/collection/customer-type/B2C/store/ica-supermarket-linero-torg-id_15172/products-data?skus="
-
 module.exports = class IcaHarvester {
 
     static async getCategories() {
-        let raw = await fetch("https://handla.ica.se/api/product-info/v1/store/15172/category/catalog80002")
+
+        const storeId = "15172"; // Uniqe id for ica-supermarket-linero-torg
+        let raw = await fetch(`https://handla.ica.se/api/product-info/v1/store/${storeId}/category/catalog80002`)
         let categories = await raw.json();
+
         return await categories.childCategories.map(x => x.seoUrl);
     }
 
     static async getProductIds() {
+
         let categories = await this.getCategories();
         let productIds = [];
 
         for (let category of categories) {
             let res = await fetch(`
-            https://handla.ica.se/api/content/v1/collections/facets/customer-type/B2C/store/ica-supermarket-linero-torg-id_15172/products?categories=${category}&sortBy=MYUSUALS&bb=true`);
+            https://handla.ica.se/api/content/v1/collections/facets/customer-type/B2C/store
+            /ica-supermarket-linero-torg-id_15172/products?categories=${category}&sortBy=MYUSUALS&bb=true`
+            );
             let items = await res.json();
             await productIds.push(items.items)
         }
@@ -33,25 +37,38 @@ module.exports = class IcaHarvester {
         productIds.forEach(category => {
             category.forEach(x => {
                 x["type"] === "product" ? cleanIds.push(x["id"]) : "";
-            })
+            });
         });
-        console.log(cleanIds.length);
+
         return await cleanIds;
     }
+
     static async fetchProducts() {
+
         let productIds = await this.getProductIds();
         let products = [];
+
         for (let i = 0; i < productIds.length; i += 100) {
-            let query = []; // Ica only allows 100 products on a single fetch
+            let query = [];
+            // Ica only allows you to fetch 100 products at a time 
+            // You have to send the productIDs in a string where you divide the ids with a comma, example : /products-data?skus=id,id,id
+            // To make life easier, we push 100 IDs in the query array and then just .join(",") them.
             for (let j = 0; j < (productIds.length - i <= 100 ? productIds.length - i : 100); j++) {
+                // (productIds.length - i <= 100 ? productIds.length - i : 100) is to make sure we dont go out of bounds when we loop through the array.
                 query.push(productIds[i + j])
             }
-            let raw = await fetch(`https://handla.ica.se/api/content/v1/collection/customer-type/B2C/store/ica-supermarket-linero-torg-id_15172/products-data?skus=${query.join(",")}`)
+
+            let raw = await fetch(`
+            https://handla.ica.se/api/content/v1/collection/customer-type/B2C/store
+            /ica-supermarket-linero-torg-id_15172/products-data?skus=${query.join(",")}`
+            )
+
             let res = await raw.json();
-            products = [...products, ...res]
+
+            products = [...products, ...res] // Add the newly fetched products to the products array
             console.log(products.length);
         }
-        console.log("Done");
+        console.log("Done *cough* borrowing *cough* some data from Ica Supermarket Linero Torg");
         return await products;
     }
 };
