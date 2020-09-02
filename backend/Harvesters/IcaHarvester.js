cacba3dacb671ae6c9c98924b0ebf09a4f10f8ed
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2020-09-02 22:21:07 +0200

    Fixed bad loop

--- backend/Harvesters/IcaHarvester.js
+++ backend/Harvesters/IcaHarvester.js
@@ -35,15 +35,15 @@ module.exports = class IcaHarvester {
                 x["type"] === "product" ? cleanIds.push(x["id"]) : "";
             })
         });
-
+        console.log(cleanIds.length);
         return await cleanIds;
     }
     static async fetchProducts() {
         let productIds = await this.getProductIds();
         let products = [];
-        for (let i = 0; i < productIds.length; i += products.length - i > 100 ? 100 : products.length - i - 1) {
-            let query = []; // Ica only allows to fetch 100 products on a single fetch
-            for (let j = 0; j < 100; j++) {
+        for (let i = 0; i < productIds.length; i += 100) {
+            let query = []; // Ica only allows 100 products on a single fetch
+            for (let j = 0; j < (productIds.length - i <= 100 ? productIds.length - i : 100); j++) {
                 query.push(productIds[i + j])
             }
             let raw = await fetch(`https://handla.ica.se/api/content/v1/collection/customer-type/B2C/store/ica-supermarket-linero-torg-id_15172/products-data?skus=${query.join(",")}`)
