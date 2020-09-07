de3e0fcf3c294906e9fd621b2c941ff012e8982c
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-09-07 11:57:16 +0200

    Added getCountryOfOrigin logic

--- backend/Scrubbers/WillysScrubber.js
+++ backend/Scrubbers/WillysScrubber.js
@@ -1,29 +1,34 @@
-const Scrubber = require('./Scrubber');
+const fetch = require("node-fetch");
+const Scrubber = require("./Scrubber");
+const { getRandomNumber } = require("../Shared/Helpers");
 
 module.exports = class WillysScrubber extends Scrubber {
-  static translationSchema = {
-    name = x => x.name,
-    categoryId = x => getCategoryId(x),
-    storeId = 'Willys',
-    brand = x => x.manufacturer,
-    price = x => x.priceValue,
-    pricePerUnit = x => parseFloat(x.comparePrice.replace(/,/, '.')),
-    quantityType = x => x.comparePriceUnit,
-    discount = x => x.savingsAmount,
-    labels = x => getLabels(x),
-    isEcological = x => x.labels.include('ecological'),
-    countryOfOrigin = async x => await getCountryOfOrigin(x)
-  }
+  static translateSchema = {
+    name: (x) => x.name,
+    categoryId: (x) => this.getCategoryId(x),
+    storeId: (x) => "Willys",
+    brand: (x) => x.manufacturer,
+    price: (x) => x.priceValue,
+    pricePerUnit: (x) => parseFloat(x.comparePrice.replace(/,/, ".")),
+    quantityType: (x) => x.comparePriceUnit,
+    discount: (x) => x.savingsAmount,
+    labels: (x) => this.getLabels(x),
+    isEcological: (x) => x.labels.includes("ecological"),
+    countryOfOrigin: async (x) => await this.getCountryOfOrigin(x.code),
+  };
 
   static getCategoryId(product) {
-    return 'CategoryId';
+    return "CategoryId";
   }
 
   static getLabels(product) {
-    return ['This', 'is', 'a', 'label'];
+    return ["This", "is", "a", "label"];
   }
 
-  static async getCountryOfOrigin(product) {
-    return 'Country';
+  static async getCountryOfOrigin(productCode) {
+    let productUrl = `https://www.willys.se/axfood/rest/p/${productCode}?avoidCache=${getRandomNumber()}`;
+    let rawProduct = await fetch(productUrl);
+    let product = await rawProduct.json();
+    return product.tradeItemCountryOfOrigin;
   }
-}
+};
