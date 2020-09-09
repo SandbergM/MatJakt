cc414910f2545480dc4b40f6217ebeeb79216a1c
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-09-09 09:19:59 +0200

    Added convertSize

--- backend/Scrubbers/Scrubber.js
+++ backend/Scrubbers/Scrubber.js
@@ -1,24 +1,41 @@
 module.exports = class Scrubber {
-
-    // Method that scrubs a product 
-    // based on the translateSchema in our subClass
-    static async scrubOne(product) {
-        let scrubbed = {};
-        let tschema = this.translateSchema;
-        for (let key in tschema) {
-            let scrubFunc = tschema[key];
-            scrubbed[key] = await scrubFunc(product);
-        }
-        return scrubbed;
+  // Method that scrubs a product
+  // based on the translateSchema in our subClass
+  static async scrubOne(product) {
+    let scrubbed = {};
+    let tschema = this.translateSchema;
+    for (let key in tschema) {
+      let scrubFunc = tschema[key];
+      scrubbed[key] = await scrubFunc(product);
     }
+    return scrubbed;
+  }
 
-    // Scrub a whole array of products
-    static async scrubAll(products) {
-        let scrubbed = [];
-        for (let product of products) {
-            scrubbed.push(await this.scrubOne(product));
-        }
-        return scrubbed;
+  // Scrub a whole array of products
+  static async scrubAll(products) {
+    let scrubbed = [];
+    for (let product of products) {
+      scrubbed.push(await this.scrubOne(product));
     }
+    return scrubbed;
+  }
 
-}
+  static convertSize(unit, size) {
+    switch (unit) {
+      case "hg":
+      case "dl": {
+        return size / 10;
+      }
+      case "cl": {
+        return size / 100;
+      }
+      case "g":
+      case "ml": {
+        return size / 1000;
+      }
+      default: {
+        return size;
+      }
+    }
+  }
+};
