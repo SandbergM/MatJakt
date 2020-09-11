d9f901b0c5bf72210acd59abf372d006d76d9e7a
theCodeHog <paolatosca@outlook.com>
2020-09-11 16:13:22 +0200

    added bootstrap and sass, small start on home, header and footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="container-fluid">
+      <p>&#169; 2020 MatJakt.se, Inc. All rights reserved.</p>
+    </footer>
+  );
+}
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(props) {
+
+  return (
+    <header>
+      <p>I am a Header TOOT</p>
+    </header>
+  );
+}
