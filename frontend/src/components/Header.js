4b2989f9f410a0db89285e048a65a7c8626e9f72
Ola Wiebe <eeken07@gmail.com>
2020-09-14 13:44:54 +0200

    color and style on header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Card, Button, CardTitle, CardText } from "reactstrap";
 
 export default function Header(props) {
 
   return (
     <header>
-      <div><p>Matjakt.se</p></div>
+      <div className="container2">
+        <div className="logo">Matjakt.se</div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div className="shoppingLista">Min Shopping Lista</div>
+        <div className="omOss">Om Oss</div>
+      </div>
     </header>
   );
 }
