983c903c8a7947d1732c7901502e928849f3bdf7
Hassan <pkhajeh@gmail.com>
2020-09-14 14:03:46 +0200

    I made a footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="container-fluid">
-      <p>&#169; 2020 MatJakt.se, Inc. All rights reserved.</p>
+    <footer>
+      <p className="footer-text">&#169; 2020 MatJakt.se, Inc. All rights reserved.</p>
     </footer>
   );
 }
