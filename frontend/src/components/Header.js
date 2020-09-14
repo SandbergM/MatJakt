5bbf5f223cd5f1ca55c0114f916047386e712724
Ola Wiebe <eeken07@gmail.com>
2020-09-14 14:55:15 +0200

    add modal to about us

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,51 @@
-import React from "react";
-import { Card, Button, CardTitle, CardText } from "reactstrap";
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+
 
 export default function Header(props) {
 
-  return (
-    <header>
-      <div className="container2">
-        <div className="logo">Matjakt.se</div>
-        
-        
-        <div className="omOss">Om Oss</div>
-      </div>
-    </header>
-  );
-}
+    const {
+      className
+    } = props;
+
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+
+    return (
+      <header>
+        <div className="container2">
+          <div className="logo">Matjakt.se</div>
+
+          <div className="omOss">
+            <Button className="omOss" color="#66C46C" onClick={toggle}>
+              Om Oss
+            </Button>
+          
+          </div>
+          <Modal isOpen={modal} toggle={toggle} className={className}>
+            <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+            <ModalBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={toggle}>
+                Do Something
+              </Button>{" "}
+              <Button color="secondary" onClick={toggle}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </Modal>
+        </div>
+      </header>
+    );
+  }
+
