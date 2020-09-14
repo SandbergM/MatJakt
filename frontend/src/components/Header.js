07406b557a26ef75a3c828b6a127609771606fd3
Hassan <pkhajeh@gmail.com>
2020-09-14 15:04:00 +0200

    Add text about us

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -27,13 +27,8 @@ export default function Header(props) {
           <Modal isOpen={modal} toggle={toggle} className={className}>
             <ModalHeader toggle={toggle}>Om MatJakt.se</ModalHeader>
             <ModalBody>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Hej!
+               Välkomna! Vi är en väldigt omtänktsam företag som vill hjälpa er att hitta bra mat med bra pris. 
             </ModalBody>
             <ModalFooter>
               
