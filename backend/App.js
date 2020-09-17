07f2906b29ffa44822be641558348e8f23bf29de
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2020-09-17 10:37:15 +0200

    Controller/Routes/Translator clean-up

--- backend/App.js
+++ backend/App.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const Translator = require("./Shared/Translator");
+const Translator = require("./Shared/CategoryTranslator");
 
 //Harvesters
 const CoopHarvester = require("./harvesters/CoopHarvester");
@@ -20,7 +20,6 @@ const Store = require("./models/store");
 const { Product, TempProduct } = require("./models/product");
 const CategoryTranslation = require("./models/categoryTranslation");
 
-
 //Routes
 const productRoutes = require("./routes/ProductRoutes");
 productRoutes(app);
@@ -32,7 +31,6 @@ const storeRoutes = require("./routes/StoreRoutes");
 storeRoutes(app);
 const categorytranslationsRoutes = require("./routes/categoryTranslationRoutes");
 categorytranslationsRoutes(app);
-
 
 //connect to MongoDB with mongoose
 const dbURI =
@@ -51,18 +49,26 @@ async function updateDatabase() {
 
   setTimeout(() => {
     Translator.fetchCategories();
-  }, 3000)
+  }, 3000);
 
   let startTime = Date.now();
   let coop = await CoopHarvester.getAllProducts();
   let coopScrubbed = await CoopScrubber.scrubAll(coop);
-  console.log(" Coopharvesting completed", coopScrubbed.length, Date.now() - startTime);
+  console.log(
+    " Coopharvesting completed",
+    coopScrubbed.length,
+    Date.now() - startTime
+  );
   products.push(...coopScrubbed);
 
   startTime = Date.now();
   let willys = await WillysHarvester.getAllProducts();
   let willysScrubbed = await WillysScrubber.scrubAll(willys);
-  console.log(" Willysharvesting completed", willysScrubbed.length, Date.now() - startTime);
+  console.log(
+    " Willysharvesting completed",
+    willysScrubbed.length,
+    Date.now() - startTime
+  );
   products.push(...willysScrubbed);
 
   startTime = Date.now();
@@ -70,20 +76,23 @@ async function updateDatabase() {
   let ica = await IcaHarvester.getAllProducts();
   let icaScrubbed = await IcaScrubber.scrubAll(ica);
   console.log(icaScrubbed);
-  console.log(" Icasharvesting completed", icaScrubbed.length, Date.now() - startTime);
+  console.log(
+    " Icasharvesting completed",
+    icaScrubbed.length,
+    Date.now() - startTime
+  );
   products.push(...icaScrubbed);
 
   startTime = Date.now();
 
-  let db = mongoose.connection.db;
+  // let db = mongoose.connection.db;
 
-  await TempProduct.collection.insertMany(products);
+  // await TempProduct.collection.insertMany(products);
 
-  console.log("Database write took : ", Date.now() - startTime);
-  startTime = Date.now();
-  Product.collection.drop();
-  await db.collection("tempproducts").rename("products");
-  console.log("Database down for : ", Date.now() - startTime);
-
+  // console.log("Database write took : ", Date.now() - startTime);
+  // startTime = Date.now();
+  // Product.collection.drop();
+  // await db.collection("tempproducts").rename("products");
+  // console.log("Database down for : ", Date.now() - startTime);
 }
 updateDatabase();
--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -2,36 +2,16 @@
 const mongoose = require("mongoose"),
   Product = mongoose.model("product");
 
-exports.get_all_products = function (req, res) {
+exports.getAllProducts = function (req, res) {
   Product.find({}, function (err, product) {
     if (err) res.send(err);
     res.json(product);
   });
 };
 
-exports.create_one_product = function (req, res) {
-  var new_product = new Product(req.body);
-  new_product.save(function (err, product) {
-    if (err) res.send(err);
-    res.json(product);
-  });
-};
-
-exports.get_one_product = function (req, res) {
+exports.findProductById = function (req, res) {
   Product.findById(req.params.taskId, function (err, task) {
     if (err) res.send(err);
     res.json(task);
   });
-};
-
-exports.update_one_product = function (req, res) {
-  Product.findOneAndUpdate(
-    { _id: req.params.taskId },
-    req.body,
-    { new: true },
-    function (err, task) {
-      if (err) res.send(err);
-      res.json(task);
-    }
-  );
 };
