3998b3da6faf37c97975aedb13329f3ee43fa23c
Ola Wiebe <eeken07@gmail.com>
2020-09-21 10:56:05 +0200

    print when click print button

--- frontend/src/components/BottomButtons.js
+++ frontend/src/components/BottomButtons.js
@@ -1,13 +1,32 @@
 import React from "react";
 import { Button, Col, Row, Container } from "reactstrap";
+import ReactDOM from "react-dom";
+import ReactToPdf from "react-to-pdf";
 
 export default function BottomButtons() {
+
+  function print() {
+    window.print()
+  }
+  
+  const ref = React.createRef();
+
   return (
-    <div className="row">
+    <div className="row" ref={ref}>
       <Col className="text-center">
-        <Button className="b1 mr-5">SAVE TO DESKTOP</Button>
-        <Button className="b1 ml-5">PRINT</Button>
+        <ReactToPdf targetRef={ref} filename="div-blue.pdf">
+          {({ toPdf }) => (
+            <Button className="b1 mr-5" onClick={toPdf}>
+              SAVE TO DESKTOP
+            </Button>
+          )}
+        </ReactToPdf>
+        <Button className="b1 ml-5" onClick={print}>
+          PRINT
+        </Button>
       </Col>
     </div>
   );
+
+  
 }
