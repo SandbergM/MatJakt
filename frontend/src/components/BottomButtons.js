1352323b11d710d971301cd22eda3ab8df9f0dc2
Ola Wiebe <eeken07@gmail.com>
2020-09-21 10:20:53 +0200

    make buttons centered with padding

--- frontend/src/components/BottomButtons.js
+++ frontend/src/components/BottomButtons.js
@@ -3,19 +3,10 @@ import { Button, Col, Row, Container } from "reactstrap";
 
 export default function BottomButtons() {
   return (
-    <div className="row justify-content-center mb-3">
-      {/* <Col lg="12" lg="4">
-        <Button>SAVE</Button>
-      </Col>
-      <Col lg="12" lg="4">
-        <Button>PRINT</Button>
-      </Col> */}
-      <Col lg="2">
-        <Button className="b1">SAVE TO DESKTOP</Button>
-      </Col>
-      <Col lg="4"></Col>
-      <Col xs lg="2">
-        <Button className="b1">PRINT</Button>
+    <div className="row">
+      <Col className="text-center">
+        <Button className="b1 mr-5">SAVE TO DESKTOP</Button>
+        <Button className="b1 ml-5">PRINT</Button>
       </Col>
     </div>
   );
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,8 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <p className="footer-text bigDiv mb-3">&#169; 2020 MatJakt.se, Inc. All rights reserved.</p>
-    
+      <p className="footer-text bigDiv mt-1 p-1">&#169; 2020 MatJakt.se, Inc. All rights reserved.</p>
     </footer>
   );
 }
