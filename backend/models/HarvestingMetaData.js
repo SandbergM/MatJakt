36e85527a4104be9e945865286ef1c238977fc87
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-09-22 08:13:05 +0200

    Added id

--- backend/models/HarvestingMetaData.js
+++ backend/models/HarvestingMetaData.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const harvestingMetaData = new Schema({
-  lastHarvestTime: { type: Number, required: true},
-  harvesterIsRunning: { type: Boolean, required: true }
+  _id: { type: Number, required: true },
+  lastHarvestTime: { type: Number, required: true },
+  harvesterIsRunning: { type: Boolean, required: true },
 });
 
-module.exports = mongoose.model("harvestingMetaData", harvestingMetaData)
+module.exports = mongoose.model("harvestingMetaData", harvestingMetaData);
