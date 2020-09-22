be01549f16130b4fecbecf3b1abe635767015674
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-09-22 08:23:21 +0200

    Added option to db connection and added run method

--- backend/Server.js
+++ backend/Server.js
@@ -5,26 +5,28 @@ const app = express();
 module.exports = class Server {
   dbURI =
     "mongodb+srv://matjakt:FoodHunt123@mat-jakt.mpf5m.mongodb.net/mat-jakt?retryWrites=true&w=majority";
-  constructor () {
-    this.run();
-  }
-  
+  constructor() {  }
+
   // Connects to db and starts express server
   async run() {
     await this.connectToDb();
-    console.log("Connected to db...")
     this.startServer();
   }
-
+  
   async connectToDb() {
     console.log("Connecting to db...");
-    await mongoose.connect(this.dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(this.dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+    console.log("Connected to db...");
   }
 
   startServer() {
     console.log("Starting server...");
     app.listen(3000, () => {
       console.log("Listening at port 3000...");
-    })
+    });
   }
 };
