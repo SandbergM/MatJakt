e9658ea178ffe23d57b80e15776cdf819f9fc039
theCodeHog <paolatosca@outlook.com>
2020-09-26 10:04:16 +0200

    more style fixes

--- frontend/src/components/ProductToBeSearched.js
+++ frontend/src/components/ProductToBeSearched.js
@@ -1,20 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ShoppingListContext } from "../contexts/ShoppingListContext";
-import { MdDelete } from "react-icons/md";
+import { MdDelete, MdEdit } from "react-icons/md";
 
 export default function ProductToBeSearched(props) {
   const { removeProductToShoppingList } = useContext(ShoppingListContext);
+
+  const [isBeingEdited, setIsBeingEdited] = useState(false);
+
+  function editProduct() {}
+
   return (
     <div className="col-12 mt-2 mb-2 product-to-be-searched-item matjaktDarkGreen-text">
       <div className="row">
         <div className="col-11 align-items-center">
-          {props.product.name}  - {props.product.quantity} {props.product.quantityType ? props.product.quantityType : ""}
-          {props.product.isEcological ? " Ekologisk" : ""}
+          {props.product.name} - {props.product.quantity}{" "}
+          {props.product.quantityType ? props.product.quantityType : ""}
+          {props.product.isEcological ? " - Ekologisk" : ""}
         </div>
-        <div
-          className="col-1 d-flex justify-content-around align-content-center pointer align-items-center"
-          onClick={() => removeProductToShoppingList(props.product)}>
-          <MdDelete />
+
+        <div className="col-1 mb-1 thomas-list-icons pointer">
+          <MdEdit onClick={editProduct()} />
+          <MdDelete
+            className="ml-1"
+            onClick={() => removeProductToShoppingList(props.product)}
+          />
         </div>
       </div>
     </div>
