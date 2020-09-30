421be4bc775b85456334fddc1ec950cb0635f91c
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2020-09-30 17:09:52 +0200

    rework on categoryTranslator

--- backend/Harvesters/WillyHarvester.js
+++ backend/Harvesters/WillyHarvester.js
@@ -1,21 +1,22 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 module.exports = class WillysHarvester {
-
   static bustCache() {
-    return '?avoidCache=' + (Math.random() + '').split('.')[1];
+    return "?avoidCache=" + (Math.random() + "").split(".")[1];
   }
 
   static async getCategories() {
-    let raw = await fetch('https://www.willys.se/leftMenu/categorytree'
-      + this.bustCache());
+    let raw = await fetch(
+      "https://www.willys.se/leftMenu/categorytree" + this.bustCache()
+    );
     let categories = await raw.json();
-    return await categories.children.map(x => x.url)
+    return await categories.children.map((x) => x.url);
   }
 
   static async getProducts(categoryURL) {
-    let raw = await fetch('https://www.willys.se/c/'
-      + categoryURL + this.bustCache() + '&size=10000');
+    let raw = await fetch(
+      "https://www.willys.se/c/" + categoryURL + this.bustCache() + "&size=3"
+    );
     return (await raw.json()).results;
   }
 
@@ -24,7 +25,7 @@ module.exports = class WillysHarvester {
     let products = [];
     for (let category of categories) {
       let x = await this.getProducts(category);
-      products = [...products, ...x]
+      products = [...products, ...x];
     }
     products = this.removeDuplicates(products);
     return products;
@@ -33,4 +34,4 @@ module.exports = class WillysHarvester {
   static removeDuplicates(products) {
     return Object.values(products.reduce((a, c) => (a[c.code] = c) && a, {}));
   }
-}
+};
