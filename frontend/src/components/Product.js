f87a121a806561b1318fff00f12a8375579ed550
Ola Wiebe <eeken07@gmail.com>
2020-10-02 10:16:59 +0200

    add button and some logic

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import { MdDelete, MdAddCircle, MdRemoveCircle } from "react-icons/md";
+import { Button } from 'reactstrap';
+import Shoppinglist from './ShoppingList'
 
 
 export default function Product(props) {
+  const [toadd, setToadd] = useState(['hej', 'hallå'])
 
+  function add() {
+    console.log('add to shoppinglist');
+    
+  }
   return (
     <div className="row product">
+      <Shoppinglist toadd={toadd}/>
       <div
         className="col-3 product-image"
-        style={{ backgroundImage: `url(${props.product.imageUrl})` }}>
+        style={{ backgroundImage: `url(${props.product.imageUrl})` }}
+      >
         <div className="icons">
           <div className="add">
             <MdAddCircle className="white-icon" />
@@ -25,7 +35,7 @@ export default function Product(props) {
         <div className="row align-items-center mt-1">
           <div className="col comparison-price">
             <span className="product-brand">{props.product.brand} </span> |
-              <span className="comparison-text"> Jfp: </span>
+            <span className="comparison-text"> Jfp: </span>
             {props.product.pricePerUnit} SEK / {props.product.comparisonUnit}
           </div>
         </div>
@@ -35,11 +45,15 @@ export default function Product(props) {
             <span className="price-text"> {props.product.price} SEK</span>
           </div>
         </div>
+        <Button className="add-button" onClick={add}>
+          Lägg till
+        </Button>
         <div className="divider mt-3 mb-2"></div>
       </div>
       <div className="delete align-items-center right-icons">
         <div className="delete amount">{}</div>
         <MdDelete />
       </div>
-    </div>)
+    </div>
+  );
 }
