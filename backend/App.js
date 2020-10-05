bfad5dd2ea822e486ae1e6a31e758fd9a488e6e2
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2020-10-06 00:49:55 +0200

    Cleanup before merge

--- backend/App.js
+++ backend/App.js
@@ -1,15 +1,10 @@
 const Server = require("./Server");
 const HarvesterScheduler = require("./HarvestScheduler");
-const t = require("./Shared/Translator");
-let wH = require("./Harvesters/WillyHarvester");
-let wS = require("./Scrubbers/WillysScrubber");
-const Translation = require('./models/translation');
-let fs = require('fs');
 
 class App {
   constructor() {
     this.server = new Server();
-    // this.harvesterScheduler = new HarvesterScheduler();
+    this.harvesterScheduler = new HarvesterScheduler();
     this.run();
   }
 
@@ -17,9 +12,7 @@ class App {
     console.log("Running server...");
     await this.server.run();
     console.log("Running harvester...");
-    // this.harvesterScheduler.run();
-    let x = await wH.getAllProducts()
-    let y = await wS.scrubAll(x);
+    this.harvesterScheduler.run();
   }
 }
 
