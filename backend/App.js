6d4abbf19fcff5934a743336daab1075aaddb654
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2020-10-05 23:51:31 +0200

    Strange bugg, App.js was deleted according to dev...

--- backend/App.js
+++ backend/App.js
@@ -3,18 +3,18 @@ const HarvesterScheduler = require("./HarvestScheduler");
 
 
 class App {
-  constructor() {
-    this.server = new Server();
-    this.harvesterScheduler = new HarvesterScheduler();
-    this.run();
-  }
+    constructor() {
+        this.server = new Server();
+        this.harvesterScheduler = new HarvesterScheduler();
+        this.run();
+    }
 
-  async run() {
-    console.log("Running server...");
-    await this.server.run();
-    console.log("Running harvester...");
-    this.harvesterScheduler.run();
-  }
+    async run() {
+        console.log("Running server...");
+        await this.server.run();
+        console.log("Running harvester...");
+        this.harvesterScheduler.run();
+    }
 }
 
 new App();
