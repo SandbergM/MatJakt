454da9efc050a5fa2b9ab0177789f950fdb2789d
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-10-07 09:18:20 +0200

    added lean

--- backend/services/productService.js
+++ backend/services/productService.js
@@ -2,9 +2,11 @@ const { Product } = require("../models/product");
 
 module.exports = class ProductService {
   static async findProductsByQuery(query) {
-    const products = await Product.find(query).sort({
-      pricePerUnit: "asc",
-    });
+    const products = await Product.find(query)
+      .sort({
+        pricePerUnit: "asc",
+      })
+      .lean();
     return products;
   }
 };
