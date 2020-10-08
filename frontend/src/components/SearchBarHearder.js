d20828cc8f1ee7bdae1c8d27416d88aa7d455543
Ola Wiebe <eeken07@gmail.com>
2020-10-08 09:57:43 +0200

    Auto stash before merge of "dev" and "origin/dev"

---
+++ frontend/src/components/SearchBarHearder.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Questionmark from "./Questionmark";
+
+
+export default function SearchBarHeader(props) {
+
+    return (
+        <div id="matjakt-searchbar-header" className="matjaktDarkGreen-bg col-12">
+            <p id="searchbar-header-title" className="matjaktWhite-text align-self-center">Sök efter produkt</p>
+        <Questionmark/>
+        </div>
+    );
+}
